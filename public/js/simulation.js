0db2f217dfa70e022366939ce8d0137588eb406b
FumiyaHorikago <h0r1k4g0@gmail.com>
2022-01-19 11:49:49 +0900

    合計金額四捨五入

--- public/js/simulation.js
+++ public/js/simulation.js
@@ -205,7 +205,7 @@ $(function () {
         price *= element;
       });
       $('.odometer').text(price);
-      $('#chooseData').append('<input type="hidden" name="price" value="' + Number(price) + '" class="send_data">');
+      $('#chooseData').append('<input type="hidden" name="price" value="' + Math.round(Number(price)) + '" class="send_data">');
     }) //通信が失敗したとき
     .fail(function (error) {
       console.log(error);

7b07a12e4bd882124a28b8f0667087e011cac69f
FumiyaHorikago <h0r1k4g0@gmail.com>
2022-01-19 12:03:01 +0900

    noindex追加

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name='robots' content='noindex,nofollow'>
     <title>住宅見積シミュレーション</title>
 
     <!-- Fonts -->
